0b919d5a3321e5693c16874b828af4328289ba4c
david319 <larapineda2003@gmail.com>
2022-03-29 15:30:38 -0600

    Creación de la clase Gaseoso

---
+++ src/Gaseoso.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+
+public class Gaseoso extends Planeta {
+    public Gaseoso(String nombre, double size, double peso, double x, double y) {
+        super(nombre,size,peso,x,y);
+    }
+    @Override
+    public boolean getProbabilidad() {
+        Random r= new Random();
+        int prob= r.nextInt(100)+1;
+        System.out.println("La probabilidad es de: " + prob);
+
+        return prob <= 20;
+    }
+
+}
