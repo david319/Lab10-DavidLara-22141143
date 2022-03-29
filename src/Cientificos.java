0f15365653ebbd761996c62b95126ab4bae0df23
david319 <larapineda2003@gmail.com>
2022-03-29 15:28:23 -0600

    Creación de la clase Cientificos

---
+++ src/Cientificos.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Cientificos {
+    // Atributos
+    ArrayList<Planeta> Descubrimientos;
+    String name;
+
+    public Cientificos(String name) {
+        Descubrimientos = new ArrayList<>();
+        this.name = name;
+    }
+
+    public ArrayList<Planeta> getDescubrimientos() {
+        return Descubrimientos;
+    }
+
+    public Planeta Encontrar(String nombre) {
+        for (Planeta p : Descubrimientos) {
+            if (nombre.equals(p.getNombre())) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void AddPlaneta(Planeta planet) {
+        Descubrimientos.add(planet);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
